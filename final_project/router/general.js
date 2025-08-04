bcde4b20fba204ff2a38f7c2cc0b12fe624756a4
Siyabonga Samson Lukhele <samsonlukhele76@gmail.com>
2025-08-04 14:06:50 +0200

    Implement author search functionality in book router

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -30,8 +30,28 @@ public_users.get("/isbn/:isbn", function (req, res) {
 
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  // Get the author from request parameters
+  const author = req.params.author;
+
+  // Get all the keys for the 'books' object
+  const bookKeys = Object.keys(books);
+
+  // Array to store matching books
+  let matchingBooks = [];
+
+  // Iterate through the 'books' array & check if author matches
+  bookKeys.forEach((key) => {
+    if (books[key].author.toLowerCase() === author.toLowerCase()) {
+      matchingBooks.push(books[key]);
+    }
+  });
+
+  // Check if any books were found
+  if (matchingBooks.length > 0) {
+    res.send(JSON.stringify(matchingBooks, null, 4));
+  } else {
+    return res.status(404).json({ message: "No books found by this author" });
+  }
 });
 
 // Get all books based on title
